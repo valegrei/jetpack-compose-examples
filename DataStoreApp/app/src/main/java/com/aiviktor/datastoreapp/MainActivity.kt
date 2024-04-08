76f3b901ce2665a093a20410044fad5420611e1d
valegrei <victoralegre@uni.pe>
2024-04-07 22:14:06 -0500

    Ejemplo de Dark Mode con DataStore

--- DataStoreApp/app/src/main/java/com/aiviktor/datastoreapp/MainActivity.kt
+++ DataStoreApp/app/src/main/java/com/aiviktor/datastoreapp/MainActivity.kt
@@ -3,26 +3,51 @@ package com.aiviktor.datastoreapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.aiviktor.datastoreapp.ui.theme.DataStoreAppTheme
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DataStoreAppTheme {
+            val darkModeStore = StoreDarkMode(this)
+            val darkMode = darkModeStore.getDarkMode.collectAsState(initial = false)
+
+            DataStoreAppTheme(
+                darkTheme = darkMode.value
+            ) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(darkModeStore, darkMode.value)
                 }
             }
         }
@@ -30,17 +55,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Greeting(darkModeStore: StoreDarkMode, darkMode: Boolean) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val dataStore = StoreUserEmail(context)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DataStoreAppTheme {
-        Greeting("Android")
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var email by rememberSaveable {
+            mutableStateOf("")
+        }
+        val userEmail = dataStore.getEmail.collectAsState(initial = "")
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            keyboardOptions = KeyboardOptions().copy(keyboardType = KeyboardType.Email)
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Button(onClick = {
+            scope.launch {
+                dataStore.saveEmail(email)
+            }
+        }) {
+            Text(text = "Guardar Email")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = userEmail.value)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            scope.launch {
+                if(darkMode){
+                    darkModeStore.saveDarkMode(false)
+                }else{
+                    darkModeStore.saveDarkMode(true)
+                }
+            }
+        }) {
+            Text(text = "Cambiar a dark")
+        }
+
+        Switch(checked = darkMode, onCheckedChange = {isChecked ->
+            scope.launch {
+                darkModeStore.saveDarkMode(isChecked)
+            }
+        })
     }
 }
