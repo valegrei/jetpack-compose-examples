778c45ebf69b9f645372bcbb6cfa88574c25238b
valegrei <victoralegre@uni.pe>
2024-05-20 22:24:34 -0500

    TabsView y LoginView

---
+++ FirebaseNotes/app/src/main/java/com/aiviktor/firebasenotes/views/login/RegisterView.kt
@@ -0,0 +1,9 @@
+package com.aiviktor.firebasenotes.views.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterView(){
+    Text(text = "Register view")
+}
